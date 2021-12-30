ae79f156b66f9d6dc4340901ae556487f5cba145
gnthibault <gnthibault@gmail.com>
2021-12-30 19:59:15 +0100

    Making proper config for sudo stuff

--- install_prerequisite.sh
+++ install_prerequisite.sh
@@ -7,3 +7,13 @@ apt-get install qemu-system-arm
 apt-get install qemu-user-static
 systemctl restart systemd-binfmt.service
 
+# from https://stackoverflow.com/questions/19383887/how-to-use-sudo-in-build-script-for-gitlab-ci
+#You can grant sudo permissions to the gitlab-runner user as this is who is executing the build script.
+#$ sudo usermod -a -G sudo gitlab-runner
+#You now have to remove the password restriction for sudo for the gitlab-runner user.
+#Start the sudo editor with
+#$ sudo visudo
+#Now add the following to the bottom of the file
+#gitlab-runner ALL=(ALL) NOPASSWD: ALL
+# You may also prefer specifying which command the sudo user can use witout pwd
+# gitlab-runner ALL=(ALL) NOPASSWD: /usr/bin/npm
--- mount_image.sh
+++ mount_image.sh
@@ -20,19 +20,19 @@ echo "Just created temporary mount dir $tmp_dir"
 # Now perform mount
 loop_dir=$(sudo losetup --show -fP "${image_iso}")
 echo "Mounting image $image_iso partitions into $loop_dir"
-mount ${loop_dir}p2 $tmp_dir
-mount ${loop_dir}p1 $tmp_dir/boot/
+sudo mount ${loop_dir}p2 $tmp_dir
+sudo mount ${loop_dir}p1 $tmp_dir/boot/
 
 # do stuff to $tmp_dir which is rpi filesystem
 echo "Starting actual image build"
 cp --remove-destination /etc/resolv.conf $tmp_dir/etc/
 cp -r $install_dir $tmp_dir/root/install_dir
-chroot $tmp_dir /bin/bash /root/install_dir/run_pi_install_script.sh
+sudo chroot $tmp_dir /bin/bash /root/install_dir/run_pi_install_script.sh
 
 # cleanup
 echo "Cleaning up temporary build directory $tmp_dir"
-umount $tmp_dir/boot/
-umount $tmp_dir
+sudo umount $tmp_dir/boot/
+sudo umount $tmp_dir
 rmdir $tmp_dir
 
 # build the xz
